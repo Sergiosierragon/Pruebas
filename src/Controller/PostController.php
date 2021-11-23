a91f95a1125cf388e3f25391863a1a6400f4f9ba
sergio <sergio@bravafabrics.com>
2021-11-23 16:15:24 +0100

    third commit

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,19 +1,28 @@
 <?php
 
-
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class PostController
+class PostController extends AbstractController
 {
     /**
      * @Route()
      */
-    public function  home()
+    public function home()
     {
-        return new Response("Hola mundo");
+        return new Response('Hola mundo');
+    }
+
+    /**
+         * @Route("/blog/{post}")
+     */
+    public function showPost($post)
+    {
+        return $this->render('posts/post.html.twig',[
+            'title'=>ucwords(str_replace('-',' ',$post))
+        ]);
     }
 }
