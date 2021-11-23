6d05ffa454d74d40f5638e3b4554618e8d4605bc
sergio <sergio@bravafabrics.com>
2021-11-23 14:49:48 +0100

    second commit

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,16 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class PostController
 {
-
+    /**
+     * @Route()
+     */
+    public function  home()
+    {
+        return new Response("Hola mundo");
+    }
 }
